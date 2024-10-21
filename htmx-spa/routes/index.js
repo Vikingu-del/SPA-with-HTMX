98bfcede109025777eee7f4d289fa8047a1db1a2
segfault <rk.seferi@gmail.com>
2024-10-22 01:30:50 +0200

    adding articles with a form

--- htmx-spa/routes/index.js
+++ htmx-spa/routes/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import articles from '../data/articles.js';
 
@@ -7,5 +8,23 @@ router.get('/', (req, res) => {
   res.render('index', {title: 'Product Listing', articles: articles});
 });
 
-export default router;
+router.get('/articles/:id', (req, res) => {
+  const article = articles.find(a => a.id === parseInt(req.params.id));
 
+  res.render('article', { title: article.name, article });
+});
+
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  };
+  articles.push(article);
+
+  res.render('partials/list', { articles });
+});
+
+export default router;
